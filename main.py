f3ca1d0aac842241a668fce7b74f7a629af443c4
PyTom <107923033+PyyTom@users.noreply.github.com>
2024-11-20 15:17:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def par_impar(numero):
+    if numero%2==0:
+        print(str(numero)+' es PAR.\nLos numeros pares desde '+str(numero)+' hasta 0 son:')
+        for n in range(numero,-1,-2):print(n)
+    else:
+        print(str(numero) + ' es IMPAR.\nLos numeros impares desde ' +str(numero) + ' hasta 1 son:')
+        for n in range(numero, 0, -2): print(n)
+def check():
+    try:
+        numero=int(input('Ingresar un numero entero: '))
+        par_impar(numero)
+    except ValueError:
+        print('No es un numero entero.')
+        check()
+check()
